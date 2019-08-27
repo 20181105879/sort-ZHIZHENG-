b79c41869bd7b74614f878596b404a0e699f1c21
20181105879 <2605753145@qq.com>
2019-08-27 17:42:38 +0800

    sort
    sort

--- sort(ZHIZHENG)/main.cpp
+++ sort(ZHIZHENG)/main.cpp
@@ -1,15 +1,41 @@
-//
-//  main.cpp
-//  sort(ZHIZHENG)
-//
-//  Created by s20181105879 on 2019/8/27.
-//  Copyright © 2019 s20181105879. All rights reserved.
-//
 
 #include <iostream>
+using namespace std;
+int n=10;
+void ssortthp(int s[],int n);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+   int *s=new int [n];
+    for(int i=0;i<n;i++)
+    {
+       cin>>s[i];
+    //   s[i]=1000-(i+1)*8;
+    }
+    ssortthp(s,n);
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<s[i]<<endl;
+    }
+    
+    
     return 0;
 }
+void ssortthp(int s[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n-i-1;j<n-1;j++)
+        {
+            if(s[j]>s[j+1])
+            {
+                int t=s[j+1];
+                s[j+1]=s[j];
+                s[j]=t;
+            }
+        }
+    }
+
+}
+
